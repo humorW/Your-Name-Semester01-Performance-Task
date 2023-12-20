bac3ac9c22de189d9cc0650f1cd3b3e63c3eb3d4
humorW <yawu11@s.sfusd.edu>
2023-12-20 11:21:15 -0800

    world scene setup

--- Player.gd
+++ Player.gd
@@ -1,11 +1,9 @@
 extends Sprite2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var speed = 400
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+
